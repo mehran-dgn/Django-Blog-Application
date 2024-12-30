4a5cbd70741eb0fea7bb1d2eee39b6915e594211
DESKTOP-35LIN5P\samsepiol <mehranthewebdev@gmail.com>
2024-12-30 19:24:16 +0330

    urls implemented , a basic view implemented , admin interface for post model implemented

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone 
 from django.contrib.auth.models import User
-# Create your models here.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -16,6 +20,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10 , choices=STATUS_CHOICES,default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
 
     class Meta:
